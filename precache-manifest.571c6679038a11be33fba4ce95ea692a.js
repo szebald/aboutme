b7fe491abc53db4760c67671877d006f14c00049
Koncz Kristóf <konczkristof@Koncz-MacBook-Air.local>
2019-07-31 10:24:53 +0200

    Updates

---
+++ precache-manifest.571c6679038a11be33fba4ce95ea692a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02781c3539e2b439cf67fe61a6cfbc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77e1620d932cdfc70b56",
+    "url": "/static/css/main.42e944f9.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "77e1620d932cdfc70b56",
+    "url": "/static/js/main.8e9d1371.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
